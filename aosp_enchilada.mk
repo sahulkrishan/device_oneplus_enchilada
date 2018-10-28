5e86ab430fdb31899a383e132c26a67ba76cb372
Akash <akashniki@gmail.com>
2018-10-28 18:02:55 +0000

    enchilada: Add Maintainer Prop

--- aosp_enchilada.mk
+++ aosp_enchilada.mk
@@ -28,6 +28,10 @@ CUSTOM_BUILD_TYPE := OFFICIAL
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# Maintainer Prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS="Akash Srivastava"
+
 PRODUCT_NAME := aosp_enchilada
 PRODUCT_DEVICE := enchilada
 PRODUCT_MANUFACTURER := OnePlus
