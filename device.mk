LineageOS/android_device_oneplus_enchilada

enchilada: Add notification LED related overlays
Change-Id: I229b626136cc7c63f83436cd3cd1ba0e960a945e

device.mk
@@ -26,7 +26,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal

sahulkrishan/device_oneplus_enchilada

Signed-off-by: Akash Srivastava <akashniki@gmail.com>

device.mk
@@ -26,7 +26,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pe
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal